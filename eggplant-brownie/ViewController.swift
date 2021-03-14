67f74635c76b9a3de5143746bc1f7721aadbfe72
Jonattan <jonattan.sousa@gmail.com>
2021-03-14 18:45:39 -0300

    primeiro commit do projeto

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //var nomeTexField: UITextField = UITextField() //instanciamos, mas não é a mais correta para o nosso uso no momento
+    @IBOutlet var nomeTexField: UITextField! // Indicamos pro Swift que esta variável terá um valor
+    @IBOutlet var felicidadeTextField: UITextField!
+    
+    
+    @IBAction func adicionar(_ sender: Any) {
+        let nome = nomeTexField.text
+        let felicidade = felicidadeTextField.text
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("comi \(nome) e fiquei com felicidade: \(felicidade)")
     }
-
-
+    
 }
 
+// Obs: O Swift não é fortemente tipado, ou seja, não seria necessário explicitar que é String
