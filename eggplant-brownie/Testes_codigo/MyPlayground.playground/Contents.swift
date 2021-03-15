57ed7c00afed5afc72de54e5d888dd6901d74bf0
Jonattan <jonattan.sousa@gmail.com>
2021-03-14 22:02:35 -0300

    Criado objeto de refeicao, adicionado itens de Item, implementado funcao totalDeCalorias e chamado no print (Tudo comentado)

--- eggplant-brownie/Testes_codigo/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/Testes_codigo/MyPlayground.playground/Contents.swift
@@ -1,4 +1,5 @@
 // Usando orientacao a objeto e metodo construtor. Criando e printando objetos.
+// Criando/ instanciando objeto lista da classe (Item) dentro da classe (Refeicao) e manipulando seus valores
 
 import UIKit
 
@@ -14,12 +15,23 @@ class Refeicao {
         
         self.nome = nome
         self.felicidade = felicidade
+    }
+    
+    // MARK - Metodos
+    
+    func totalDeCalorias() -> Double{
+        var total = 0.0
         
+        for item in itens{
+            total += item.calorias
+        }
+        return total
     }
 
 }
 
-class Item {
+//Classe de itens
+class Item { //Seria como uma lista de itens da refeicao, ja ha um array na classe refeicao instanciada com seu tipo
     let nome: String
     let calorias: Double
     
@@ -29,17 +41,24 @@ class Item {
     }
 }
 
+//Instanciando itens com suas propriedades
 let feijao = Item(nome: "Feijao", calorias: 45.9)
 let arroz = Item(nome: "Arroz", calorias: 12)
 let contraFile = Item(nome: "Contra File", calorias: 30)
 
+//Instanciando refeicao e suas propriedades
+let refeicao = Refeicao(nome: "Almoco", felicidade: "4")
 
-//
-//let meuAlmoco = Refeicao(nome: "Lasanha", felicidade: "5")
-//let minhaJanta = Refeicao(nome: "Macarronada", felicidade: "4")
-//
-//print(meuAlmoco.nome)
-//print(meuAlmoco.felicidade)
-//
-//print(minhaJanta.nome)
-//print(minhaJanta.felicidade)
+//Adicionando a lista itens do objeto refeicao
+refeicao.itens.append(arroz)
+refeicao.itens.append(feijao)
+refeicao.itens.append(contraFile)
+
+//Printando o primeiro item da lista itens do objeto refeicao com 'if let'
+if let primeiroItemDaLista = refeicao.itens.first {
+    print(primeiroItemDaLista.nome)
+}
+
+//Printando o total de calorias ao chamar o metodo de calculo dentro da classe Refeicao, todo objeto tem o metodo de calculo. ;)
+print(refeicao.totalDeCalorias())
+
